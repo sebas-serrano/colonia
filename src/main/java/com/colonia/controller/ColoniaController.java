ffd030c39dd3400588a5be9a278fad3b61663b63
cupa1 <cupa1@ar.ibm.com>
2021-03-08 15:57:01 -0300

    Varios

--- src/main/java/com/colonia/controller/ColoniaController.java
+++ src/main/java/com/colonia/controller/ColoniaController.java
@@ -3,18 +3,19 @@ package com.colonia.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.colonia.model.Alumno;
 import com.colonia.service.AlumnoService;
 
-@Controller
+@RestController
+@RequestMapping(value="/api")
 public class ColoniaController {
 
 	@Autowired
