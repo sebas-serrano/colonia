791085c5c0c7f3a76f605ca8e4c135f145ab4b57
chancha-santafe <78742073+chancha-santafe@users.noreply.github.com>
2021-03-19 12:35:43 -0300

    Bugs

--- src/main/java/com/colonia/ColoniaApplication.java
+++ src/main/java/com/colonia/ColoniaApplication.java
@@ -2,10 +2,18 @@ package com.colonia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ColoniaApplication {
+public class ColoniaApplication extends SpringBootServletInitializer{
 	
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(ColoniaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ColoniaApplication.class, args);
