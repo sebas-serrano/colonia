04e2f8a2f387f909fc3aaf1a0e7bd88352852858
sebas <SSerranoAvila@proveedor.teco.com.ar>
2021-08-15 01:24:41 -0300

    Updated

--- src/test/java/com/colonia/ColoniaApplicationTests.java
+++ src/test/java/com/colonia/ColoniaApplicationTests.java
@@ -2,31 +2,27 @@ package com.colonia;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.colonia.model.Usuario;
-import com.colonia.repo.IUsuarioRepo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ColoniaApplicationTests {
 	
-	@Autowired
-	private IUsuarioRepo repo;
+//	@Autowired
+//	private IUsuarioRepo repo;
 
-	@Autowired
-	private BCryptPasswordEncoder encoder;
+//	@Autowired
+//	private BCryptPasswordEncoder encoder;
 
 	@Test
 	public void contextLoads() {
-		Usuario usuario= new Usuario();
-		usuario.setPassword(encoder.encode("admin"));
-		usuario.setId(1);
-		usuario.setNombre("colonia");
-		repo.save(usuario);
+//		Usuario usuario= new Usuario();
+//		usuario.setPassword(encoder.encode("admin"));
+//		usuario.setId(1);
+//		usuario.setNombre("colonia");
+////		repo.save(usuario);
+//		assertTrue(true);
 	}
 
 }
